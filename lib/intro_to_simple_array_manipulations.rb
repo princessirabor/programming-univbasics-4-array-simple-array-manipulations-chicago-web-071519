72c47136b979da5d71b6616b09eac769c94bc6d5
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 22:16:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,7 @@ def using_concat(arr1,arr2)
   arr1.concat(arr2)
 end
 def using_insert(array,element)
-  array.insert(3,element)
+  array.insert(4,element)
 end
 def using_uniq(array)
   array.uniq 
